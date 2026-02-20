8c5f55c52ec0f6becc4fb0bda15900bbb9df194a
Sarthak Sharma <sarthak1081@gmail.com>
2026-02-20 10:44:33 +0530

    UC2

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -2,6 +2,19 @@ package Palindrome;
 
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Hello palindreme");
+        String text= "madam";
+        boolean ispalindrome = true;
+        for (int i=0;i< text.length()/2;i++){
+               if(text.charAt(i) != text.charAt(text.length() -1 -i)){
+                   ispalindrome= false;
+                   break;
+               }
+        }
+
+
+        System.out.println("INPUT TEXT:" +text );
+        System.out.println("is it a palindrome? :"+ ispalindrome);
+
+
     }
 }
