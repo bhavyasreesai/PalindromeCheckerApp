e948b6b4a5a984e07efd8e07806c6be5c48a0996
Sarthak Sharma <sarthak1081@gmail.com>
2026-02-20 10:16:24 +0530

    UC1

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -2,6 +2,10 @@ package Palindrome;
 
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Hello palindreme");
+        System.out.println("Welcome to the Palindrome Checker Management System ");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized Successfully");
     }
 }
+
+
