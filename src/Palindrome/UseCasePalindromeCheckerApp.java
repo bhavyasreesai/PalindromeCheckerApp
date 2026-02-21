7df51aabcf174d66f96b9e2d6c19e18480b9c62c
C BHAVYA SREE SAI <chinasanibhavya@gmail.com>
2026-02-21 22:27:41 +0530

    UC5

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -1,15 +1,21 @@
 package Palindrome;
+
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
+
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
-        String input = "noon";
+        String input = "civic";
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
         boolean isPalindrome = true;
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
