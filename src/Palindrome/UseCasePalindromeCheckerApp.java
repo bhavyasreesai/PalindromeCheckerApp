41b8c87e7e5ae75021bbe0b01becdb71c725d95f
C BHAVYA SREE SAI <chinasanibhavya@gmail.com>
2026-02-21 22:48:47 +0530

    UC7

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -1,7 +1,23 @@
 package Palindrome;
 
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Hello palindreme");
+        String input = "refer";
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+        boolean isPalindrome = true;
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
